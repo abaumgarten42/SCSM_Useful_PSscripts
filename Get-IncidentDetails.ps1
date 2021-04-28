b933c93251a9aea5f182c3f19bb342600864be22
Andreas Baumgarten <abaumgarten@outlook.com>
2021-04-28 22:19:53 +0200

    Update

--- Get-IncidentDetails.ps1
+++ Get-IncidentDetails.ps1
@@ -16,3 +16,17 @@ $CreatedDate = $IRobject.CreatedDate
 $LastModifiedDate = $IRobject.LastModified
 $Classification = $IRobject.Classification.DisplayName
 $SupportGroup = $IRobject.TierQueue.DisplayName
+
+# Get related objects of Incident objects
+# Get relationships
+$relAffectedUser = Get-SCSMRelationshipClass -Name System.WorkItemAffectedUser
+$relAssignToUser = Get-SCSMRelationshipClass -Name System.WorkItemAssignedToUser
+$relAffectedConfigItems = Get-SCSMRelationshipClass -Name System.WorkItemAboutConfigItem
+# Get related object - Affected User
+$affectedUserDisplayName = (Get-SCSMRelatedobject -SMObject $IRobject -Relationship $relAffectedUser).DisplayName
+$affectedUserUserName = (Get-SCSMRelatedobject -SMObject $IRobject -Relationship $relAffectedUser).UserName
+# Get related object - Assigned To User
+$assignedToUserDisplayName = (Get-SCSMRelatedobject -SMObject $IRobject -Relationship $relAssignToUser).DisplayName
+$assignedToUserUserName = (Get-SCSMRelatedobject -SMObject $IRobject -Relationship $relAssignToUser).UserName
+# Get affected Config Items
+$affectedConfigItemsDisplayName = (Get-SCSMRelatedobject -SMObject $IRobject -Relationship $relAffectedConfigItems).DisplayName
