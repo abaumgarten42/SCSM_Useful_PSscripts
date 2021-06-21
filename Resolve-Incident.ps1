1d5deeb8c3feba8b9d0a54e9f3bda8b0b5c91952
[Andreas Baumgarten] <abaumgarten@outlook.com>
2021-06-21 18:26:03 +0200

    Upload

---
+++ Resolve-Incident.ps1
@@ -0,0 +1,32 @@
+Import-Module SMLets
+# Set some variables
+$irId="IR2046" # Incident ID
+$resolvedByUserName = "ppan1234" # Resolved By UserName
+# Resolution details
+$resolutionDescription = "Just a test" # Resolution Description
+$resolutionCategory = "Fixed By Analyst" # Resolution Category
+
+# Get required SCSM classes
+$irClass = Get-SCSMClass -Name System.workitem.Incident$
+$userClass = Get-SCSMclass -Name System.Domain.User$
+# Get relationship class
+$resolvedByUserRel = Get-SCSMRelationshipClass -name System.WorkItem.TroubleTicketResolvedByUser$
+# Get required SCSM objects
+# Incident object
+$irObj = Get-SCSMObject -Class $irClass -Filter "ID -eq $irId"
+# ResolvedBy User object
+$resolvedByUserObj = Get-SCSMObject -Class $userClass -Filter "UserName -eq $resolvedByUserName"
+
+# Prepar resolution details 
+$resolveDetails=@{
+"Status" = "Resolved";
+"ResolutionDescription" = "$resolutionDescription";
+"ResolutionCategory" = "$resolutionCategory";
+"ResolvedDate" = (Get-Date).ToUniversalTime();
+"TargetResolutionTime" = (Get-Date).ToUniversalTime();
+}
+# Update incident object with seolution details
+Set-SCSMObject -SMObject $irObj -PropertyHashtable $resolveDetails
+
+# Create new relationship object
+New-SCSMRelationshipObject -Relationship $resolvedByUserRel -Source $irObj -Target $resolvedByUserObj -Bulk
