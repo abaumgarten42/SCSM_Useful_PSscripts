98ddffdf1d27f53304c7019a9961bd246943abfa
[Andreas Baumgarten] <abaumgarten@outlook.com>
2021-07-06 22:41:03 +0200

    Initial

---
+++ Create-ManualActivity.ps1
@@ -0,0 +1,33 @@
+# Create a Manual Activiy with different properties and AssignedTo User
+
+Import-Module SMlets # Implort SMLets module
+
+# Define Manual Activity properties
+$maTitle = "Test MA by PowerShell 1"
+$maDescription = "Test Description"
+$maPriority = "Medium"
+$maArea = "Other"
+$maStatus = "In Progress"
+$maAssignedToUser = "ppan1234"
+# --------------------
+$smdefaultserver = "SCSM1" # Define SCSM Management Server
+$MAclass = Get-SCSMclass -name System.Workitem.Activity.ManualActivity$ # Get SCSM Manual Activity class object
+$UserClass = Get-SCSMClass -name System.Domain.User$ # Get SCSM User class object
+$relAssignedToUser = Get-SCSMRelationshipClass -Name System.WorkItemAssignedToUser # Get SCSM relationship AssignedTo User
+#  Get AssignedTo User  
+$maAssignedToUserObj = Get-SCSMObject -Class $UserClass -Filter "UserName -eq $maAssignedToUser"
+# Prepare Manual Activity properties
+$properties = @{
+    Id          = "MA{0}"
+    Title       = $maTitle
+    Description = $maDescription
+    Priority    = $maPriority
+    Area        = $maArea
+    Status      = $maStatus
+}
+# Create Manual Activity object
+$newMA = New-SCSMObject -Class $MAclass -PropertyHashtable $properties -PassThru
+# Set AssignedTo User
+if ($maAssignedToUserObj -and $newMA) {
+    New-SCSMRelationshipObject -RelationShip $relAssignedToUser -Source $newMA -Target $maAssignedToUserObj -Bulk
+}
